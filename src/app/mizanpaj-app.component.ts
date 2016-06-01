79df776f81ed0a69ed6ae22389bf49ecd8d7ba7e
zafer.kayikcilar <zafer.kayikcilar@aptech.com.tr>
2016-06-01 10:07:04 +0300

    Crop

--- src/app/mizanpaj-app.component.ts
+++ src/app/mizanpaj-app.component.ts
@@ -18,14 +18,67 @@ export class MizanpajAppAppComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var canvas = new fabric.Canvas('c');
-    var src = "../../images/kupurSecond.jpg";
-
-    fabric.Image.fromURL("../../images/kupurSecond.jpg", function (oImg) {
+    let canvas = new fabric.Canvas('c');
+    let src = "../../images/kupurSecond.jpg";
+    // Seçimin Başladığı ve Bittiği Koordinatlar
+    let firstX, firstY, lastX, lastY;
+    // Seçimin Başladığı ve Bittiği Koordinatlara Göre Max & Min
+    let minX, minY, maxX, maxY;
+    // Küpürü Yükle
+    fabric.Image.fromURL(src, function (oImg) {
       oImg.left = 10;
       oImg.top = 10;
       canvas.add(oImg);
       canvas.renderAll();
-    }, { hasControls: false, selectable: false, evented: false });
+    }, { hasControls: false, selectable: false, evented: false, strokeDashArray: [2, 2], opacity: 1 });
+    // Seçimin Başladığı Koordinatlar
+    canvas.on('mouse:down', function (e) {
+      firstX = getMouseCoords(e)[0];
+      firstY = getMouseCoords(e)[1];
+    });
+    // Seçimin Bittiği Koordinatlar
+    canvas.on('mouse:up', function (e) {
+      lastX = getMouseCoords(e)[0];
+      lastY = getMouseCoords(e)[1];
+      getRectCoords();
+
+      canvas.clipTo = function (ctx) {
+        ctx.rect(minX, minY, maxX - minX, maxY - minY);
+      }
+
+    });
+    // Min & Max Değerleri (Dikdörtgen Koordinatları)
+    function getRectCoords() {
+      if (firstX < lastX) {
+        minX = firstX;
+        maxX = lastX;
+      }
+      else {
+        minX = lastX;
+        maxX = firstX;
+      }
+
+      if (firstY < lastY) {
+        minY = firstY;
+        maxY = lastY;
+      }
+      else {
+        minY = lastY;
+        maxY = firstY;
+      }
+
+      console.log(minX + ' ' + maxX + ' ' + minY + ' ' + maxY);
+    }
+
+    function getMouseCoords(event) {
+      var pointer = canvas.getPointer(event.e);
+      var posX = pointer.x;
+      var posY = pointer.y;
+      let XY = [posX, posY];
+      return XY;
+    }
+
+
+
   }
 }
